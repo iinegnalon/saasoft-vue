9c38cde9f73bf669576120db5ab37f65f464e02b
Viktor <iinegnalon@gmail.com>
2025-09-10 17:03:28 +0400

    Init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+  <main class="home-page">
+    <div>Home page</div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.home-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
