ca7c039ddeceba951d1885471c2b023f5a5f35dc
Viktor <iinegnalon@gmail.com>
2025-09-10 17:18:56 +0400

    Vuetify install

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,13 @@
 <template>
   <main class="home-page">
     <div>Home page</div>
+    <v-btn color="primary">Test Vuetify</v-btn>
+    <div>
+      <v-text-field
+        label="Test Vuetify"
+        placeholder="Test Vuetify"
+      ></v-text-field>
+    </div>
   </main>
 </template>
 
